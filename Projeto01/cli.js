5ada3e9366773c273237ae8cb7fcef86c83f2a3f
Guilherme Alves <79064185+GuiIhermeLandim@users.noreply.github.com>
2022-05-25 09:22:13 -0300

    Add files via upload

---
+++ Projeto01/cli.js
@@ -0,0 +1,11 @@
+import chalk from "chalk";
+import pegarArquivo from "./index.js";
+
+const caminho = process.argv;
+
+async function processaTexto(caminhoDoArquivo) {
+    const resultado = await pegarArquivo(caminho[2]);
+    console.log(chalk.yellow("Lista de Links"), resultado);
+}
+
+processaTexto(caminho);
